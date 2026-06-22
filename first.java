8472e8e244e2b44fdf06050724c0999c7b732968
Shivam <shivampipraiya572@gmail.com>
2026-06-22 14:34:44 +0530

    little workout

--- first.java
+++ first.java
@@ -2,5 +2,10 @@
   void main(){
     IO.println("new update");
     IO.println("HEy there is your githhub");
+    System.out.println(add(2,5));
+    }
+
+    static int  add(int a, int b){
+      return a+b;
     }
 
